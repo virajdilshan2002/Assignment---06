9954c766e67ded392e92ad0681cadd9f69ec0f1e
virajdilshan2002 <virajdilshan2019@gmail.com>
2024-11-10 21:58:17 +0530

    into single pages

---
+++ js/page-navigations.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  function showPage(page) {
+    $(".page").removeClass("active"); // Hide all pages
+    $("#" + page).addClass("active"); // Show selected page
+  }
+
+  function checkHash() {
+    const page = window.location.hash.substr(1) || "home";
+    showPage(page);
+  }
+
+  // Initial load based on hash or default to "home"
+  checkHash();
+
+  // Update content on hash change
+  $(window).on("hashchange", function () {
+    checkHash();
+  });
+
+  // Change hash when button is clicked
+  $("button").on("click", function () {
+    const page = $(this).data("page");
+    window.location.hash = page;
+  });
+});
